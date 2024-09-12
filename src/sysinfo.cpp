84974ba3576632c44c5f69d5ba288a7f4768bdf7
Voltaged <140754373+VoltagedDebunked@users.noreply.github.com>
2024-09-12 17:02:04 +0300

    Fixed sysinfo for all OS's

--- src/sysinfo.cpp
+++ src/sysinfo.cpp
@@ -2,26 +2,27 @@
 #include <vector>
 #include <string>
 
-// Forward declarations
-void windows_specific_function();
-void unix_specific_function();
-std::vector<std::string> get_system_info();
-
-// Platform-specific function implementations
+// Platform-specific headers
 #ifdef _WIN32
     #include <windows.h>
     #define OS_WINDOWS
-#elif defined(__unix__) || defined(__APPLE__)
+#elif defined(__APPLE__)
     #include <unistd.h>
     #include <sys/utsname.h>
     #include <sys/sysctl.h>
-    #define OS_UNIX
+    #define OS_MACOS
+#elif defined(__linux__)
+    #include <unistd.h>
+    #include <sys/utsname.h>
+    #include <sys/sysinfo.h>
+    #define OS_LINUX
 #else
     #error "Unsupported operating system"
 #endif
 
-typedef void (*SystemFunction)();
-SystemFunction get_system_function();
+// Forward declarations
+void platform_specific_function();
+std::vector<std::string> get_system_info();
 
 int main() {
     std::cout << "OS Detection and System Information" << std::endl;
@@ -29,13 +30,13 @@ int main() {
 
     #ifdef OS_WINDOWS
         std::cout << "Running on Windows" << std::endl;
-    #elif defined(OS_UNIX)
-        std::cout << "Running on Unix-like system" << std::endl;
+    #elif defined(OS_MACOS)
+        std::cout << "Running on macOS" << std::endl;
+    #elif defined(OS_LINUX)
+        std::cout << "Running on Linux" << std::endl;
     #endif
 
-    // Simulated dynamic linking
-    SystemFunction sys_func = get_system_function();
-    sys_func();
+    platform_specific_function();
 
     // Get additional system info
     std::vector<std::string> sys_info = get_system_info();
@@ -48,7 +49,7 @@ int main() {
 }
 
 #ifdef OS_WINDOWS
-void windows_specific_function() {
+void platform_specific_function() {
     SYSTEM_INFO sysInfo;
     GetSystemInfo(&sysInfo);
     std::cout << "Windows-specific function:" << std::endl;
@@ -72,13 +73,12 @@ std::vector<std::string> get_system_info() {
 
     return info;
 }
-#elif defined(OS_UNIX)
-void unix_specific_function() {
+#elif defined(OS_MACOS)
+void platform_specific_function() {
     long numCPU = sysconf(_SC_NPROCESSORS_ONLN);
-    std::cout << "Unix-specific function:" << std::endl;
+    std::cout << "macOS-specific function:" << std::endl;
     std::cout << "Number of processors: " << numCPU << std::endl;
 
-    // Use sysctl for macOS to get memory information
     int mib[2] = { CTL_HW, HW_PHYSMEM };
     uint64_t memSize;
     size_t len = sizeof(memSize);
@@ -100,12 +100,40 @@ std::vector<std::string> get_system_info() {
 
     return info;
 }
+#elif defined(OS_LINUX)
+void platform_specific_function() {
+    long numCPU = sysconf(_SC_NPROCESSORS_ONLN);
+    std::cout << "Linux-specific function:" << std::endl;
+    std::cout << "Number of processors: " << numCPU << std::endl;
+
+    struct sysinfo memInfo;
+    sysinfo(&memInfo);
+    long long totalVirtualMem = memInfo.totalram;
+    totalVirtualMem += memInfo.totalswap;
+    totalVirtualMem *= memInfo.mem_unit;
+
+    std::cout << "Total virtual memory: " << totalVirtualMem / (1024 * 1024) << " MB" << std::endl;
+}
+
+std::vector<std::string> get_system_info() {
+    std::vector<std::string> info;
+    struct utsname unameData;
+    uname(&unameData);
+
+    info.push_back("System name: " + std::string(unameData.sysname));
+    info.push_back("Node name: " + std::string(unameData.nodename));
+    info.push_back("Release: " + std::string(unameData.release));
+    info.push_back("Version: " + std::string(unameData.version));
+    info.push_back("Machine: " + std::string(unameData.machine));
+
+    return info;
+}
 #endif
 
 SystemFunction get_system_function() {
     #ifdef OS_WINDOWS
         return windows_specific_function;
-    #else
-        return unix_specific_function;
+    #elif defined(OS_MACOS) || defined(OS_LINUX)
+        return platform_specific_function;
     #endif
 }
